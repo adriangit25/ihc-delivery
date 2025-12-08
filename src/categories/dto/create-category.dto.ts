de3f52ef8f9fdaba07455c32dd7c9e39c95cd0da
adriangit25 <adrenher2468@gmail.com>
2025-12-08 10:36:33 -0500

    Recien Hacer

--- src/categories/dto/create-category.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsNotEmpty, IsString } from "class-validator";
-import { ApiProperty } from "@nestjs/swagger";
-
-export class CreateCategoryDto {
-  @ApiProperty({ example: "Bebidas" })
-  @IsString()
-  @IsNotEmpty()
-  name: string;
-}
